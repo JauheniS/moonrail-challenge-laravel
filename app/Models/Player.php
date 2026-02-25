a642521b00c97dc3d9ea4e1b25248a230b8bf8c4
Scopic Interviewing <noreply@codeaid.io>
2026-02-25 10:55:37 +0000

    Initial commit

---
+++ app/Models/Player.php
@@ -0,0 +1,43 @@
+<?php
+
+// /////////////////////////////////////////////////////////////////////////////
+// PLEASE DO NOT RENAME OR REMOVE ANY OF THE CODE BELOW. 
+// YOU CAN ADD YOUR CODE TO THIS FILE TO EXTEND THE FEATURES TO USE THEM IN YOUR WORK.
+// YOU SHOULD NOT CHANGE THE DATABASE STRUCTURE, ADDING NEW FIELDS, RENAMING OR REMOVING THE CURRENT FIELDS MAY RESULT IN A FAILED TEST
+// /////////////////////////////////////////////////////////////////////////////
+
+namespace App\Models;
+
+use App\Enums\PlayerPosition;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+/**
+ * @property integer $id
+ * @property string $name
+ * @property PlayerPosition $position
+ * @property PlayerSkill $skill
+ */
+class Player extends Model
+{
+    use HasFactory;
+
+    protected $guarded = ['id'];
+
+    protected $fillable = [
+        'name',
+        'position'
+    ];
+
+    protected $casts = [
+        'position' => PlayerPosition::class
+    ];
+
+    protected $with = ['skills'];
+
+    public function skills(): HasMany
+    {
+        return $this->hasMany(PlayerSkill::class);
+    }
+}
